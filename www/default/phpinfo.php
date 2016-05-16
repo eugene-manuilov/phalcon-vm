e88c714351882093e19d0909d8ab502bc82ebee8
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-05-16 12:15:31 +0300

    Added phpMyAdmin config and added PHP info page

---
+++ www/default/phpinfo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Phalcon VM Dashboard</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+    <body>
+        <?php phpinfo(); ?>
+    </body>
+</html>
