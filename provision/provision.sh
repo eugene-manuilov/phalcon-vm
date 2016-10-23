9ecffb2768d118e3824ff8487dd4203a334a83b1
Eugene Manuilov <eugene.manuilov@gmail.com>
2016-10-24 02:46:52 +0300

    Updated provision script to use latest version of puppet

--- provision/provision.sh
+++ provision/provision.sh
@@ -6,19 +6,23 @@
 # the commands `vagrant up`, `vagrant provision`, or `vagrant reload` are used.
 
 # installs puppet and its modules if it is not installed yet
-if ! dpkg -s puppet > /dev/null 2>&1; then
+if [ ! -f /opt/puppetlabs/bin/puppet ]; then
+	pushd /tmp/
+	wget http://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+	dpkg -i puppetlabs-release-pc1-xenial.deb
 	apt-get update --quiet --yes
-	apt-get install --quiet --yes puppet
+	apt-get install --quiet --yes puppetserver
+	popd
 
-	puppet module install puppetlabs-stdlib
-	puppet module install puppetlabs-apt
-	puppet module install puppetlabs-vcsrepo
-	puppet module install puppetlabs-inifile
-	puppet module install computology-packagecloud
-	puppet module install saz-memcached
-	puppet module install puppet-nginx
+	/opt/puppetlabs/bin/puppet module install puppetlabs-stdlib
+	/opt/puppetlabs/bin/puppet module install puppetlabs-apt
+	/opt/puppetlabs/bin/puppet module install puppetlabs-vcsrepo
+	/opt/puppetlabs/bin/puppet module install puppetlabs-inifile
+	/opt/puppetlabs/bin/puppet module install computology-packagecloud
+	/opt/puppetlabs/bin/puppet module install saz-memcached
+	/opt/puppetlabs/bin/puppet module install puppet-nginx
 fi
 
 # concatinates all puppet scripts into init.pp file
 cat /srv/provision/puppet/partials/*.pp > /srv/provision/puppet/init.pp
-puppet apply /srv/provision/puppet/init.pp
+/opt/puppetlabs/bin/puppet apply /srv/provision/puppet/init.pp
