09de9977037e65a075ff1e766a4e8ed7a5a37607
Eugene Manuilov <eugene.manuilov@gmail.com>
2016-10-24 11:43:32 +0300

    Addd grunt-cli and gulp-cli packages install to provisioning

--- provision/puppet/partials/06-tools.pp
+++ provision/puppet/partials/06-tools.pp
@@ -14,21 +14,32 @@ vcsrepo { '/srv/www/default/public/opcache-status':
 	depth    => 1,
 }
 
-# Bower
-exec { 'bower-install':
-	command => '/usr/bin/npm install -g bower',
-	creates => '/usr/local/bin/bower',
-    require => Package['npm'],
-}
-
+# NPM packages
 file { '/usr/bin/node':
 	ensure  => 'link',
 	target  => '/usr/bin/nodejs',
 	require => Package['nodejs'],
 }
 
-# Composer
+exec { 'npm-install-bower':
+	command => '/usr/bin/npm install -g bower',
+	creates => '/usr/local/bin/bower',
+    require => [ Package['npm'], File['/usr/bin/node'] ],
+}
 
+exec { 'npm-install-grunt':
+	command => '/usr/bin/npm install -g grunt-cli',
+	creates => '/usr/local/bin/grunt',
+	require => Package['npm'],
+}
+
+exec { 'npm-install-gulp':
+	command => '/usr/bin/npm install -g gulp-cli',
+	creates => '/usr/local/bin/gulp',
+	require => Package['npm'],
+}
+
+# Composer
 exec { 'composer-config':
 	command     => '/usr/bin/composer -q global config bin-dir /usr/local/bin',
 	environment => 'COMPOSER_HOME=/usr/local/src/composer',
