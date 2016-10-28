ca963f272153e7d55570e98623917bcf425f97fa
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-10-28 15:54:43 +0300

    Added versions to puppet modules

--- provision/provision.sh
+++ provision/provision.sh
@@ -9,14 +9,14 @@ if [ ! -f /opt/puppetlabs/bin/puppet ]; then
 	apt-get install --quiet --yes puppetserver
 	popd
 
-	/opt/puppetlabs/bin/puppet module install puppetlabs-stdlib
-	/opt/puppetlabs/bin/puppet module install puppetlabs-apt
-	/opt/puppetlabs/bin/puppet module install puppetlabs-vcsrepo
-	/opt/puppetlabs/bin/puppet module install puppetlabs-inifile
-	/opt/puppetlabs/bin/puppet module install computology-packagecloud
-	/opt/puppetlabs/bin/puppet module install saz-memcached
+	/opt/puppetlabs/bin/puppet module install puppetlabs-stdlib --version 4.13.1
+	/opt/puppetlabs/bin/puppet module install puppetlabs-apt --version 2.3.0
+	/opt/puppetlabs/bin/puppet module install puppetlabs-vcsrepo --version 1.4.0
+	/opt/puppetlabs/bin/puppet module install puppetlabs-inifile --version 1.6.0
+	/opt/puppetlabs/bin/puppet module install computology-packagecloud --version 0.3.1
+	/opt/puppetlabs/bin/puppet module install saz-memcached --version 2.8.1
 	/opt/puppetlabs/bin/puppet module install puppet-nginx --version 0.4.0
-	/opt/puppetlabs/bin/puppet module install puppet-archive
+	/opt/puppetlabs/bin/puppet module install puppet-archive --version 1.1.2
 fi
 
 # concatinates all puppet scripts into init.pp file
