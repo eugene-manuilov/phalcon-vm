08dc94bf82e6fff49256d8b8960fcf8cd03eccd2
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-10-30 11:12:37 +0200

    Split tools into two groups

--- www/default/assets/js/application.js
+++ www/default/assets/js/application.js
@@ -35,6 +35,12 @@
 		self.nasty = false;
 		self.menu = phalconvm.menu;
 
+		self.notEmptyGroup = function(group) {
+			var emptyObject = angular.equals({}, group.items),
+				emptyArray = angular.equals([], group.items);
+			return ! emptyArray && ! emptyObject;
+		};
+
 		self.setNasty = function() {
 			self.nasty = true;
 		};
