476d614d56665b57d7d5b5a87ced6e438e82d3a1
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-11-04 12:20:14 +0200

    Added ability to set root password and activate logs for mysql user

--- provision/puppet/modules/phalconvm/manifests/mysql.pp
+++ provision/puppet/modules/phalconvm/manifests/mysql.pp
@@ -1,8 +1,32 @@
-class phalconvm::mysql( $enabled = false ) {
+class phalconvm::mysql(
+	$enabled                       = false,
+	$password                      = 'root',
+	$general_log                   = false,
+	$slow_query_log                = true,
+	$log_queries_not_using_indexes = true,
+	$long_query_time               = 1,
+) {
 	if $enabled == true {
 		class { '::mysql::server':
-			root_password           => 'root',
+			root_password           => $password,
 			remove_default_accounts => true,
+			override_options        => {
+				'mysqld' => {
+					'general_log'                   => $general_log ? {
+						true => 'on',
+						default => 'off',
+					},
+					'slow_query_log'                => $slow_query_log ? {
+						true => 'on',
+						default => 'off',
+					},
+					'long_query_time'               => $long_query_time,
+					'log_queries_not_using_indexes' => $log_queries_not_using_indexes ? {
+						true => 'on',
+						default => 'off',
+					},
+				},
+			}
 		}
 	} else {
 		service { 'mysql':
