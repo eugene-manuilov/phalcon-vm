223a56dd1c90a00c25108f7141fba215bf2ae417
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-11-08 23:46:10 +0200

    Updated sidebar styles and added "add new site" element

--- www/default/app/controllers/IndexController.php
+++ www/default/app/controllers/IndexController.php
@@ -11,6 +11,7 @@ class IndexController extends \Phalcon\Mvc\Controller {
 		$this->tag->setTitle( 'Phalcon VM' );
 
 		$this->assets->addCss( '//fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic', false );
+		$this->assets->addCss( '//fonts.googleapis.com/icon?family=Material+Icons', false );
 		$this->assets->addCss( 'css/app.css' );
 
 		$this->assets->addInlineJs( sprintf( 'var phalconvm = %s;', json_encode( $phalconvm ) ) );
