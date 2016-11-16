ace16c0168e7c3a0e753defbaacf7a2661a3405e
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-11-16 21:09:37 +0200

    Updated website class to populate root folder and nginx config for the site

--- provision/puppet/modules/phalconvm/manifests/website.pp
+++ provision/puppet/modules/phalconvm/manifests/website.pp
@@ -1,9 +1,31 @@
 class phalconvm::website( $sites = [] ) {
 	$sites.each |$site| {
-		file {"/srv/www/${site[directory]}/htdocs":
+		file { "/srv/www/${site[directory]}":
 			ensure => 'directory',
-            owner  => 'www-data',
-            group  => 'www-data',
 		}
-    }
+
+		file { "/srv/www/${site[directory]}/htdocs":
+			ensure  => 'directory',
+            require => File["/srv/www/${site[directory]}"],
+		}
+
+		nginx::resource::vhost { $site[label]:
+			ensure      => present,
+			server_name => split($site[domains], ' '),
+			www_root    => "/srv/www/${site[directory]}/htdocs/public",
+			index_files => ['index.php'],
+			try_files   => ['$uri', '$uri/', '/index.php?$args'],
+			locations   => {
+				"${site[label]}-php-loc" => {
+					ensure        => present,
+					location      => '~ \.php$',
+					try_files     => ['$uri', '=404'],
+					fastcgi       => 'phpupstream',
+					fastcgi_param => {
+						'SCRIPT_FILENAME' => '$document_root$fastcgi_script_name'
+					},
+				}
+			},
+		}
+	}
 }
