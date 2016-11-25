77024036859c0980839faccb1dab986cc13cc9e3
Eugene Manuilov <eugene.manuilov@get10up.com>
2016-11-25 15:21:15 +0200

    Fixed misprint in the module installation script.

--- provision/puppet/modules/phalconvm/manifests/utils/puppet_modules.pp
+++ provision/puppet/modules/phalconvm/manifests/utils/puppet_modules.pp
@@ -2,7 +2,7 @@ define phalconvm::utils::puppet_modules( $modules ) {
 	$modules.each |$module, $version| {
 		exec { "${name}-${module}":
 			command => "/opt/puppetlabs/bin/puppet module install ${name}-${module} --version ${version}",
-			creates => "/etc/puppetlabs/code/environments/production/modules/${modules}/",
+			creates => "/etc/puppetlabs/code/environments/production/modules/${module}/",
 		}
 	}
 }
